e6f4cedd692b4e8cb306a3c519583fa4fc90bb01
shan <shan@iosdeMacBook-Pro.local>
2022-11-16 21:59:08 +0800

    1.0

---
+++ lab4/Input.h
@@ -0,0 +1,48 @@
+//
+// Created by shan on 2022/11/16.
+//
+
+#ifndef LAB4_INPUT_H
+#define LAB4_INPUT_H
+#define enable_debug
+
+#ifdef enable_debug
+#define debug(s) {cerr<< s<<endl;}
+#else
+#define debug(...){}
+#endif
+
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <dirent.h>
+#include <vector>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Input {
+    string folderPath;
+    vector<std::string> filenames;
+    string format;
+public:
+    explicit Input(const string &folderPath);
+};
+
+class InputFolder {
+    　
+private:
+    string folderPath;
+    vector<Input> ls;
+public:
+    ~InputFolder() = default;
+    explicit InputFolder(const string &folderPath);
+
+
+};
+
+void GetFileNames(std::string path, std::vector<std::string> &filenames);
+
+
+#endif //LAB4_INPUT_H
---
+++ lab4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+#include <unistd.h>
+#include "Input.h"
+
+int main() {
+    InputFolder i ("../input") ;
+
+    pause();
+    return 0;
+}
