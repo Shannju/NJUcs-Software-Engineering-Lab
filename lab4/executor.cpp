d15928f50fe0a53896372bc3d69e2df77e3f2170
Shannju <Wangshan9102@163.com>
2022-11-17 16:59:18 +0800

    1.85

--- lab4/executor.cpp
+++ lab4/executor.cpp
@@ -1,30 +1,5 @@
-//
-// Created by shan on 2022/11/17.
-//
+#include "executor.h"
 
-#include "rand.h"
+string eg::generate() {
 
-string randInt(int a, int b)
-// eg: a=1 b=3 b-a =2  123
-{
-    srand(time(NULL));
-    int ans = rand() % (b - a + 1) + a;
-    return to_string(ans);
-}
-
-char randChar() {
-    srand(time(NULL));
-    if (rand() % 2) { return rand() % (26) + 'a'; }
-    else {
-        return rand() % (26) + 'A';
-    }
-
-}
-
-string randString(int a, int b) {
-    srand(time(NULL));
-    int len = rand() % (b - a + 1) + a;
-    string ans;
-    for (int i = 0; i < len; i++) { ans += randChar(); }
-    return ans;
 }
--- lab4/executor.h
+++ lab4/executor.h
@@ -5,6 +5,18 @@
 #ifndef LAB4_EXECUTOR_H
 #define LAB4_EXECUTOR_H
 #include "Input.h"
+#include "rand.h"
+
+enum type{Int,Char,String};
+struct eg{
+    type t;
+    int a,b;
+    string generate();
+    eg(type t, int a, int b) : t(t), a(a), b(b) {}
+
+
+};
+
 
 class exeUnit{
     pair<string,string >p;
@@ -14,6 +26,7 @@ class exeUnit{
 
 };
 
+//对于一个文件夹执行测试
 class executor {
     Input * i;
     vector<string>ls;
