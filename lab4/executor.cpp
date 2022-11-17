25dd75ead432b68aa2033683885c1e204d82415c
Shannju <Wangshan9102@163.com>
2022-11-17 17:19:07 +0800

    1.86

--- lab4/executor.cpp
+++ lab4/executor.cpp
@@ -1,5 +1,60 @@
+//
+// Created by shan on 2022/11/17.
+//
 #include "executor.h"
 
 string eg::generate() {
+    switch (t) {
+        case Int:
+            return randInt(a, b);
+        case Char:
+            return randChar() + "";
+        case String:
+            return randString(a, b);
+    }
+}
 
+void executor::makeList() {
+    ifstream ifs = std::ifstream(i->getFormat(), std::ios::out);
+    if (!ifs.is_open()) {
+        debug("fail to open " + i->getFormat())
+        return;
+    }
+
+    string tmp;
+    while (ifs >> tmp) {
+        if (tmp.compare("char") == 0) {
+            egList.push_back(eg(Char, 0, 0));
+        } else if (tmp.find("int") != -1) {
+            int floor, ceiling;
+            int lbracket = tmp.find('(');
+            int comma = tmp.find(',');
+            int rbracket = tmp.find(')');
+            floor = stoi(tmp.substr(lbracket + 1, comma - lbracket - 1));
+            ceiling = stoi(tmp.substr(comma + 1, rbracket - comma - 1));
+            egList.push_back(eg(Int, floor, ceiling));
+        } else if (tmp.find("string") != -1) {
+            int floor, ceiling;
+            int lbracket = tmp.find('(');
+            int comma = tmp.find(',');
+            int rbracket = tmp.find(')');
+            floor = stoi(tmp.substr(lbracket + 1, comma - lbracket - 1));
+            ceiling = stoi(tmp.substr(comma + 1, rbracket - comma - 1));
+            egList.push_back(eg(String, floor, ceiling));
+        }
+    }
+    ifs.close();
+}
+
+string executor::makeTst() {
+    string ans;
+    for (int i = 0; i <egList.size();i++)
+    {
+        ans +=egList[i].generate()+" ";
+    }
+    return ans;
+}
+
+executor::executor(Input *i) : i(i) {
+    tstfile = i->getFolderPath() + "/tstfile.txt";
 }
--- lab4/executor.h
+++ lab4/executor.h
@@ -4,35 +4,40 @@
 
 #ifndef LAB4_EXECUTOR_H
 #define LAB4_EXECUTOR_H
+
 #include "Input.h"
 #include "rand.h"
 
-enum type{Int,Char,String};
-struct eg{
+enum type {
+    Int, Char, String
+};
+
+struct eg {
     type t;
-    int a,b;
+    int a, b;
     string generate();
-    eg(type t, int a, int b) : t(t), a(a), b(b) {}
-
-
+    eg(type t, int a, int b) : t(t), a(a), b(b) {};
 };
 
 
-class exeUnit{
-    pair<string,string >p;
-    string format;
-    bool test ();
-    string randomIns();
+class exeUnit {
+    string f0, f1;
+    bool test();//执行一次随机tst 返回结果是否一样
+    string randomIns();//生成一条shell指令
+public:
 
 };
 
 //对于一个文件夹执行测试
 class executor {
-    Input * i;
-    vector<string>ls;
-
-
-
+    Input *i;
+    vector<string> ls;
+    vector<eg> egList;
+    string tstfile;
+public:
+    void makeList();//初始化eglist
+    string makeTst();//生成测试用例文件
+    explicit executor(Input *i);
 };
 
 
