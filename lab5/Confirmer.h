5cdcfc8df5986307fec866a7eb837d0ea68c4a95
Shannju <Wangshan9102@163.com>
2022-12-01 14:18:40 +0800

    6.1

--- lab5/Confirmer.h
+++ lab5/Confirmer.h
@@ -4,9 +4,53 @@
 
 #ifndef LAB5_CONFIRMER_H
 #define LAB5_CONFIRMER_H
+#include <cstring>
+#include <fstream>
+#include "qdebug.h"
+#include <iostream>
+#include <vector>
+#include "output.h"
+using namespace std;
+#define enable_debug
+#ifdef enable_debug
+#define debug(s) {cerr<< s<<'/n';}
+#else
+#define debug(...){}
+#endif
+
+
+class ufs{
+    ufs * p;
+    string content;
+    string filename;
+public:
+    explicit ufs(const string &filename) : filename(filename) {
+        std::ifstream ifs (".."+filename,ios::in);
+        string tmp;
+        if (ifs.is_open()) {
+            while (getline(ifs,tmp)) {
+                content += tmp;
+                content += '\n';
+            }
+        } else debug("euqal.csv creation failed");
+        ifs.close();
+    }
+    void setParnet(ufs * parent){
+        while (!parent->isRoot())
+            parent=parent->getParent();
+        this->p=parent;
+    }
+    bool isRoot(){return p == this;}
+    ufs* getParent (){return p;}
+    string getContent(){return content;}
+};
+
 
 
 class Confirmer {
+    vector<ufs> ufset;
+    output o;
+
 
 };
 
---
+++ lab5/output.h
@@ -0,0 +1,27 @@
+//
+// Created by shan on 2022/11/17.
+//
+
+#ifndef LAB4_OUTPUT_H
+#define LAB4_OUTPUT_H
+#include "Input.h"
+using namespace std;
+
+
+class output {
+    ofstream equal;
+    ofstream inequal;
+    ofstream abnormal;
+    vector<string> abnormalList;
+public:
+    void writeAb();
+    void addAb(string s);
+    void add (bool e,string a,string b);
+    void addAbnormal(string a);
+    void init();
+
+    virtual ~output();
+};
+
+
+#endif //LAB4_OUTPUT_H
